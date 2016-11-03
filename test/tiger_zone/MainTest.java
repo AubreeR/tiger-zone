3a2f22770f349922052d57042a04335db87925fc
Kenan Yildirim <kenan@kenany.me>
2016-11-03 09:59:12 -0400

    set up unit testing
    Closes https://github.com/KenanY/tiger-zone/pull/1

---
+++ test/tiger_zone/MainTest.java
@@ -0,0 +1,12 @@
+package tiger_zone;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, 2);
+	}
+}
