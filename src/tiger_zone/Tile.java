dd047b24d94c7326419761e274402e1dbd15f6f7
khardin2012 <khardin2012@ufl.edu>
2016-11-12 15:12:27 -0500

    Updated Tile Class

--- src/tiger_zone/Tile.java
+++ src/tiger_zone/Tile.java
@@ -3,48 +3,71 @@ package tiger_zone;
 import java.io.File;
 
 public class Tile {
-	// TLC = Top left corner, TL = Top left, BRC = Bottom right corner, etc
-	char[] tileArr; 
-	char cent;
-	File file; 
-	Boolean pennant; 
-	int row, col;
-
-	//static Stack<Tile> stack = new Stack<>(); 
+	public Tile(char[] sides, char cent){
+		private int row;
+		private in col; 
+		private boolean shield;
+		private boolean meeple; 
+		
+	}
 	
-	public static void main(String[] args){
-		Tile t1 = new Tile(); 
-		Tile t2 = new Tile();
-		Tile t3 = new Tile();
+	public static void initTiles() {
+		// f = farm, c = city, 
+		//sides names:   TLC  TL  TM  TR TRC  RT  RM  RB BRC BR  BM   BL BLC  LB  LM  LT
+		//sides values:   1   2   3   4   5   6   7   8   9  10  11   12  13  14  15  16
+		char[] Asides = {'f','f','f','f','f','f','f','f','f','f','r','f','f','f','f','f'};
+		char[] Bsides = {'f','f','f','f','f','f','f','f','f','f','f','f','f','f','f','f'};
+		char[] Csides = {'c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'};
+		char[] Dsides = {'f','f','r','f','c','c','c','c','c','f','r','f','f','f','f','f'};
+		char[] Esides = {'c','c','c','c','c','f','f','f','f','f','f','f','f','f','f','f'};
+		char[] Fsides = {'c','f','f','f','c','c','c','c','c','f','f','f','c','c','c','c'};
+		char[] Gsides = {'c','c','c','c','c','f','f','f','c','c','c','c','c','f','f','f'};
+		char[] Hsides = {'c','f','f','f','c','c','c','c','c','f','f','f','c','c','c','c'};
+		char[] Isides = {'f','f','f','f','c','c','c','c','c','c','c','c','c','f','f','f'};
+		char[] Jsides = {'c','c','c','c','c','f','r','f','f','f','r','f','f','f','f','f'};
+		char[] Ksides = {'f','f','r','f','c','c','c','c','c','f','f','f','f','f','r','f'};
+		char[] Lsides = {'f','f','r','f','c','c','c','c','c','f','r','f','f','f','r','f'};
+		char[] Msides = {'c','c','c','c','c','f','f','f','f','f','f','f','c','c','c','c'};
+		char[] Nsides = {'c','c','c','c','c','f','f','f','f','f','f','f','c','c','c','c'};
+		char[] Osides = {'c','c','c','c','c','f','r','f','f','f','r','f','c','c','c','c'};
+		char[] Psides = {'c','c','c','c','c','f','r','f','f','f','r','f','c','c','c','c'};
+		char[] Qsides = {'c','c','c','c','c','c','c','c','c','f','f','f','c','c','c','c'};
+		char[] Rsides = {'c','c','c','c','c','c','c','c','c','f','f','f','c','c','c','c'};
+		char[] Ssides = {'c','c','c','c','c','c','c','c','c','f','r','f','c','c','c','c'};
+		char[] Tsides = {'c','c','c','c','c','c','c','c','c','f','r','f','c','c','c','c'};
+		char[] Usides = {'f','f','r','f','f','f','f','f','f','f','r','f','f','f','f','f'};
+		char[] Vsides = {'f','f','f','f','f','f','f','f','f','f','r','f','f','f','r','f'};
+		char[] Wsides = {'f','f','f','f','f','f','r','f','f','f','r','f','f','f','r','f'};
+		char[] Xsides = {'f','f','r','f','f','f','r','f','f','f','r','f','f','f','r','f'};
 		
-		// Monastery surrounded by farm and south road (tile 1)
-		char[] t1Arr = {'f','f','f','f','f','f','f','f','f','f','r','f','f','f','f','f'};
-		t1.tileArr = t1Arr;
-		t1.pennant = false;
-		t1.cent = 'm';
-		File f1 = new File("C:/Users/Kyle/Dropbox/Tiles/Tile1.png");
-		t1.file = f1; 
+		// tile name, sides  center row col meeple
+		Tile A = new Tile(Asides,'r');
+		Tile B = new Tile(Bsides,'m');
+		Tile C = new Tile(Csides,'c');
+		Tile D = new Tile(Dsides,'r');
+		Tile E = new Tile(Esides,'f');
+		Tile F = new Tile(Fsides,'c');
+		Tile G = new Tile(Gsides,'c');
+		Tile H = new Tile(Hsides,'f');
+		Tile I = new Tile(Isides,'f');
+		Tile J = new Tile(Jsides,'r');
+		Tile K = new Tile(Ksides,'r');
+		Tile L = new Tile(Lsides,'r');
+		Tile M = new Tile(Msides,'f');
+		Tile N = new Tile(Nsides,'f');
+		Tile O = new Tile(Osides,'r');
+		Tile P = new Tile(Psides,'r');
+		Tile Q = new Tile(Qsides,'c');
+		Tile R = new Tile(Rsides,'c');
+		Tile S = new Tile(Ssides,'c');
+		Tile T = new Tile(Tsides,'c');
+		Tile U = new Tile(Usides,'r');
+		Tile V = new Tile(Vsides,'r');
+		Tile W = new Tile(Wsides,'r');
+		Tile X = new Tile(Xsides,'r');
 		
-		// Monastery surrounded by farm (tile 2)
-		char[] t2Arr = {'f','f','f','f','f','f','f','f','f','f','f','f','f','f','f','f'};
-		t2.tileArr = t2Arr;
-		t2.pennant = false;
-		t2.cent = 'm'; 
-		File f2 = new File("C:/Users/Kyle/Dropbox/Tiles/Tile2.png");
-		t2.file = f2;
 		
-		// Completely city tile
-		char[] t3Arr = {'c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'};
-		t3.tileArr = t3Arr;
-		t3.pennant = true;
-		File f3 = new File("C:/Users/Kyle/Dropbox/Tiles/Tile3.png");
-		t3.file = f3;
 		
-//		stack.push(t1);
-//		stack.push(t2);
-//		stack.push(t3);
-//		Collections.shuffle(stack);
-//		ts = stack.pop(); 
-	
-
+		return;
+	}
 }
