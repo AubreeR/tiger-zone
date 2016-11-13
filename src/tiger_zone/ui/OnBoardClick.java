344d360f89313ea5a1deb0161118abd767fa5d1b
Kyle.Hardin <khardin2012@ufl.edu>
2016-11-13 15:15:55 -0500

    Updated OnBoardClick

--- src/tiger_zone/ui/OnBoardClick.java
+++ src/tiger_zone/ui/OnBoardClick.java
@@ -1,14 +1,24 @@
 package tiger_zone.ui;
 
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class OnBoardClick extends MouseAdapter {
 	public void mouseClicked(MouseEvent event) {
 		JPanel j = (JPanel)event.getSource();
-		j.setBackground(Color.YELLOW);	
+		ImageIcon img = new ImageIcon("./src/resources/tile3.png");
+		ImageIcon imgScale = new ImageIcon(img.getImage().
+				getScaledInstance(j.getHeight(), j.getWidth(), Image.SCALE_SMOOTH));
+		JLabel label = new JLabel();
+		label.setIcon(imgScale);
+		j.add(label);
+		j.revalidate();
+		//j.setBackground(Color.YELLOW);	
 	}
 }
