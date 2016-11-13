7b744cfc377ba799d5e530712288de4a589782df
Kenan Yildirim <kenan@kenany.me>
2016-11-13 15:53:18 -0500

    add `Board.createDefaultStack()` and pile getter

--- src/tiger_zone/Board.java
+++ src/tiger_zone/Board.java
@@ -2,15 +2,39 @@ package tiger_zone;
 
 import java.util.Stack;
 
+/**
+ * The <code>Board</code> class represents both the collection of placed tiles and the stack of tiles which have not yet
+ * been placed.
+ */
 public class Board {
 	private Tile[][] tileArray = new Tile[152][152];
 	private Stack<Tile> pile;
 	
-	Board(Stack<Tile> pile) {
+	/**
+	 * Creates an empty board with a particular stack of tiles.
+	 * 
+	 * @param pile Stack of unplaced tiles.
+	 */
+	public Board(Stack<Tile> pile) {
 		this.pile = pile;
 	}
 	
-	public static void initTiles() {
+	/**
+	 * Returns this board's stack of unplaced tiles.
+	 * 
+	 * @return Stack of unplaced tiles.
+	 */
+	public Stack<Tile> getPile() {
+		return this.pile;
+	}
+	
+	/**
+	 * Creates a "default stack", which contains one of each tile "in order" (e.g. tile A is at the bottom of the stack
+	 * whilst tile X is at the top of the stack).
+	 * 
+	 * @return Stack of tiles.
+	 */
+	public static Stack<Tile> createDefaultStack() {
 		// f = farm, c = city, 
 		//sides names:   TLC  TL  TM  TR TRC  RT  RM  RB BRC BR  BM   BL BLC  LB  LM  LT
 		//sides values:   1   2   3   4   5   6   7   8   9  10  11   12  13  14  15  16
@@ -39,29 +63,31 @@ public class Board {
 		char[] Wsides = {'f','f','f','f','f','f','r','f','f','f','r','f','f','f','r','f'};
 		char[] Xsides = {'f','f','r','f','f','f','r','f','f','f','r','f','f','f','r','f'};
 
-		Tile A = new Tile(Asides, 'm', "./src/resources/tile1.png");
-		Tile B = new Tile(Bsides, 'm', "./src/resources/tile2.png");
-		Tile C = new Tile(Csides, 'c', "./src/resources/tile3.png");
-		Tile D = new Tile(Dsides, 'r', "./src/resources/tile4.png");
-		Tile E = new Tile(Esides, 'f', "./src/resources/tile5.png");
-		Tile F = new Tile(Fsides, 'c', "./src/resources/tile6.png");
-		Tile G = new Tile(Gsides, 'c', "./src/resources/tile7.png");
-		Tile H = new Tile(Hsides, 'f', "./src/resources/tile8.png");
-		Tile I = new Tile(Isides, 'f', "./src/resources/tile9.png");
-		Tile J = new Tile(Jsides, 'r', "./src/resources/tile10.png");
-		Tile K = new Tile(Ksides, 'r', "./src/resources/tile11.png");
-		Tile L = new Tile(Lsides, 'r', "./src/resources/tile12.png");
-		Tile M = new Tile(Msides, 'f', "./src/resources/tile13.png");
-		Tile N = new Tile(Nsides, 'f', "./src/resources/tile14.png");
-		Tile O = new Tile(Osides, 'r', "./src/resources/tile15.png");
-		Tile P = new Tile(Psides, 'r', "./src/resources/tile16.png");
-		Tile Q = new Tile(Qsides, 'c', "./src/resources/tile17.png");
-		Tile R = new Tile(Rsides, 'c', "./src/resources/tile18.png");
-		Tile S = new Tile(Ssides, 'c', "./src/resources/tile19.png");
-		Tile T = new Tile(Tsides, 'c', "./src/resources/tile20.png");
-		Tile U = new Tile(Usides, 'r', "./src/resources/tile21.png");
-		Tile V = new Tile(Vsides, 'r', "./src/resources/tile22.png");
-		Tile W = new Tile(Wsides, 'r', "./src/resources/tile23.png");
-		Tile X = new Tile(Xsides, 'r', "./src/resources/tile24.png");
+		Stack<Tile> pile = new Stack<Tile>();
+		pile.push(new Tile(Asides, 'm', "./src/resources/tile1.png"));
+		pile.push(new Tile(Bsides, 'm', "./src/resources/tile2.png"));
+		pile.push(new Tile(Csides, 'c', "./src/resources/tile3.png"));
+		pile.push(new Tile(Dsides, 'r', "./src/resources/tile4.png"));
+		pile.push(new Tile(Esides, 'f', "./src/resources/tile5.png"));
+		pile.push(new Tile(Fsides, 'c', "./src/resources/tile6.png"));
+		pile.push(new Tile(Gsides, 'c', "./src/resources/tile7.png"));
+		pile.push(new Tile(Hsides, 'f', "./src/resources/tile8.png"));
+		pile.push(new Tile(Isides, 'f', "./src/resources/tile9.png"));
+		pile.push(new Tile(Jsides, 'r', "./src/resources/tile10.png"));
+		pile.push(new Tile(Ksides, 'r', "./src/resources/tile11.png"));
+		pile.push(new Tile(Lsides, 'r', "./src/resources/tile12.png"));
+		pile.push(new Tile(Msides, 'f', "./src/resources/tile13.png"));
+		pile.push(new Tile(Nsides, 'f', "./src/resources/tile14.png"));
+		pile.push(new Tile(Osides, 'r', "./src/resources/tile15.png"));
+		pile.push(new Tile(Psides, 'r', "./src/resources/tile16.png"));
+		pile.push(new Tile(Qsides, 'c', "./src/resources/tile17.png"));
+		pile.push(new Tile(Rsides, 'c', "./src/resources/tile18.png"));
+		pile.push(new Tile(Ssides, 'c', "./src/resources/tile19.png"));
+		pile.push(new Tile(Tsides, 'c', "./src/resources/tile20.png"));
+		pile.push(new Tile(Usides, 'r', "./src/resources/tile21.png"));
+		pile.push(new Tile(Vsides, 'r', "./src/resources/tile22.png"));
+		pile.push(new Tile(Wsides, 'r', "./src/resources/tile23.png"));
+		pile.push(new Tile(Xsides, 'r', "./src/resources/tile24.png"));
+		return pile;
 	}	
 }
---
+++ test/tiger_zone/BoardTest.java
@@ -0,0 +1,29 @@
+package tiger_zone;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Stack;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoardTest {
+	private static Stack<Tile> pile;
+	private static Board board;
+	
+	@BeforeClass
+	public static void setup() {
+		pile = new Stack<Tile>();
+		board = new Board(pile);
+	}
+
+	@Test
+	public void getPileTest() {
+		assertEquals(pile, board.getPile());
+	}
+	
+	@Test
+	public void createDefaultStackTest() {
+		assertEquals(24, Board.createDefaultStack().size());
+	}
+}
