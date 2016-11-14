f3eed8f352882c7506bfb22b5a484b30a2320e2a
Kenan Yildirim <kenan@kenany.me>
2016-11-14 13:10:53 -0500

    prefer private over protected
    There is no need for this property to be protected since no class will ever
    derive from this test class.

--- test/tiger_zone/TileTest.java
+++ test/tiger_zone/TileTest.java
@@ -6,14 +6,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TileTest {
-	protected static Tile tile;
+	private static Tile tile;
 
 	@BeforeClass
 	public static void setup() {
 		char[] sides = {'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f'};
 		tile = new Tile(sides, 'm', "./src/resources/tile2.png");
 	}
-	
+
 	@Test
 	public void getImagePath() {
 		assertEquals("./src/resources/tile2.png", tile.getImagePath());
