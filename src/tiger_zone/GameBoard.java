66b4367f8203966bb5cd8d9ece574b6058ff0b29
Kenan Yildirim <kenan@kenany.me>
2016-11-14 13:22:09 -0500

    normalize indentation
    This makes `GameBoard.java`'s indentation match that of the rest of the project.

--- src/tiger_zone/GameBoard.java
+++ src/tiger_zone/GameBoard.java
@@ -4,102 +4,102 @@ package tiger_zone;
 // need to ditch player... potentially
 // tile(setXCoord(X), setYCoord(Y));
 class BoardCell{
-  public int x;
-  public int y;
-  public Tile tile;
-  public Tiger tiger; //will omit
-  public Player player; // will omit
-  boolean isOccupied;
+	public int x;
+	public int y;
+	public Tile tile;
+	public Tiger tiger; //will omit
+	public Player player; // will omit
+	boolean isOccupied;
 
-  public BoardCell(int x, int y){
-    this.x = x;
-    this.y = y;
-    this.tile = null;
-    this.tiger = null;
-    this.player = null;
-    this.isOccupied = false;
-  }
-  public void setTiger(Tiger tiger){
-    this.tiger = tiger;
-  }
-  public void setTile(Tile tile){
-    this.tile = tile;
-  }
-  public void setPlayer(Player player){
-    this.player = player;
-  }
+	public BoardCell(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.tile = null;
+		this.tiger = null;
+		this.player = null;
+		this.isOccupied = false;
+	}
+	public void setTiger(Tiger tiger){
+		this.tiger = tiger;
+	}
+	public void setTile(Tile tile){
+		this.tile = tile;
+	}
+	public void setPlayer(Player player){
+		this.player = player;
+	}
 
-  public Tiger getTiger(){
-    return this.tiger;
-  }
-  public Tile getTile(){
-    return this.tile;
-  }
-  public Player getPlayer(){
-    return this.player;
-  }
-  public int getXCoord(){
-    return this.x;
-  }
-  public int getYCoord(){
-    return this.y;
-  }
+	public Tiger getTiger(){
+		return this.tiger;
+	}
+	public Tile getTile(){
+		return this.tile;
+	}
+	public Player getPlayer(){
+		return this.player;
+	}
+	public int getXCoord(){
+		return this.x;
+	}
+	public int getYCoord(){
+		return this.y;
+	}
 
 }
 
 public class GameBoard{
-  private final BoardCell[][] gameGrid;
+	private final BoardCell[][] gameGrid;
 
-  public GameBoard(int n,int m){
-    gameGrid = new BoardCell[n*2][m*2];
-  }
+	public GameBoard(int n,int m){
+		gameGrid = new BoardCell[n*2][m*2];
+	}
 
-  public void initializeBoard(){
-    for(int i = 0; i < gameGrid.length; ++i){
-      for(int j = 0; j <  gameGrid.length; ++j){
-        gameGrid[i][j] = new BoardCell(i,j);
-      }
-    }
-  }
-  public void addTileToBoard(Tile tile){
-    for(int i = 0; i < gameGrid.length; ++i){
-      for(int j = 0; j < gameGrid.length; ++j){
-        if(tile.x == i && tile.y == j){
-          gameGrid[i][j].setTile(tile);
-        }
-      }
-    }
-  }
-  public Tile getTileFromBoard(int x, int y){
-    return gameGrid[x][y].tile;
-  }
-
-
-
-
-  public static void main(String[] args){
-    int size = 4; // taken from "Stack size"
-
-
-    GameBoard b = new GameBoard(size, size);
-    b.initializeBoard();
-    System.out.println("The size of the Board is: " + b.gameGrid.length);
-    System.out.println("Testing GameBoard add");
-
-    Tile myTile = new Tile(1,1,"Dave");
-    b.addTileToBoard(myTile);
-    Tile result = b.getTileFromBoard(1,1);
-
-    String s1 = result.name;
-    String s2 = b.getTileFromBoard(1,1).name;
-
-    UnitTest myUnit = new UnitTest();
-    String unit = myUnit.ModTest(s1, s2);
-    System.out.println(unit); 
+	public void initializeBoard(){
+		for(int i = 0; i < gameGrid.length; ++i){
+			for(int j = 0; j <  gameGrid.length; ++j){
+				gameGrid[i][j] = new BoardCell(i,j);
+			}
+		}
+	}
+	public void addTileToBoard(Tile tile){
+		for(int i = 0; i < gameGrid.length; ++i){
+			for(int j = 0; j < gameGrid.length; ++j){
+				if(tile.x == i && tile.y == j){
+					gameGrid[i][j].setTile(tile);
+				}
+			}
+		}
+	}
+	public Tile getTileFromBoard(int x, int y){
+		return gameGrid[x][y].tile;
+	}
 
 
 
-  }
+
+	public static void main(String[] args){
+		int size = 4; // taken from "Stack size"
+
+
+		GameBoard b = new GameBoard(size, size);
+		b.initializeBoard();
+		System.out.println("The size of the Board is: " + b.gameGrid.length);
+		System.out.println("Testing GameBoard add");
+
+		Tile myTile = new Tile(1,1,"Dave");
+		b.addTileToBoard(myTile);
+		Tile result = b.getTileFromBoard(1,1);
+
+		String s1 = result.name;
+		String s2 = b.getTileFromBoard(1,1).name;
+
+		UnitTest myUnit = new UnitTest();
+		String unit = myUnit.ModTest(s1, s2);
+		System.out.println(unit);
+
+
+
+	}
 
 
 
@@ -118,16 +118,16 @@ public class GameBoard{
 
 
 class UnitTest{
-  UnitTest(){
+	UnitTest(){
 
-  }
-  public  String ModTest(String s1, String s2){
-    if(s1 == s2){
-      return "TEST PASSED";
-    }
-    else
-      return "TEST FAILED";
-  }
+	}
+	public  String ModTest(String s1, String s2){
+		if(s1 == s2){
+			return "TEST PASSED";
+		}
+		else
+			return "TEST FAILED";
+	}
 
 }
 
@@ -135,19 +135,19 @@ class UnitTest{
 
 
 class Tiger{
-  String name;
-  public Tiger(String name){
-    this.name = name;
-  }
+	String name;
+	public Tiger(String name){
+		this.name = name;
+	}
 }
 class Player{}
 class Tile{
-  int x;
-  int y;
-  String name;
-  public Tile(int x, int y, String name){
-    this. x = x;
-    this.y = y;
-    this.name = name;
-  }
+	int x;
+	int y;
+	String name;
+	public Tile(int x, int y, String name){
+		this. x = x;
+		this.y = y;
+		this.name = name;
+	}
 }
