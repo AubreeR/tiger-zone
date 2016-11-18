5916209e07238a6a7a4793e16b2c169331d21933
Kenan Yildirim <kenan@kenany.me>
2016-11-18 13:13:31 -0500

    implement tile rotation

--- test/tiger_zone/TileTest.java
+++ test/tiger_zone/TileTest.java
@@ -1,6 +1,7 @@
 package tiger_zone;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,12 +11,42 @@ public class TileTest {
 
 	@BeforeClass
 	public static void setup() {
-		char[] sides = {'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f'};
-		tile = new Tile(sides, 'm', "./src/resources/tile2.png");
+		char[] sides = {'j', 'r', 'j', 'l', 'l', 'l', 'j', 'r', 'j', 'j', 'j', 'j'};
+		tile = new Tile(sides, 'r', "./src/resources/tile20.png");
 	}
 
 	@Test
 	public void getImagePath() {
-		assertEquals("./src/resources/tile2.png", tile.getImagePath());
+		assertEquals("./src/resources/tile20.png", tile.getImagePath());
+	}
+
+	@Test
+	public void rotationTest() {
+		char[] originalSides = {'j', 'r', 'j', 'l', 'l', 'l', 'j', 'r', 'j', 'j', 'j', 'j'};
+		char[] rotatedSides1 = {'j', 'j', 'j', 'j', 'r', 'j', 'l', 'l', 'l', 'j', 'r', 'j'};
+		char[] rotatedSides2 = {'j', 'r', 'j', 'j', 'j', 'j', 'j', 'r', 'j', 'l', 'l', 'l'};
+		char[] rotatedSides3 = {'l', 'l', 'l', 'j', 'r', 'j', 'j', 'j', 'j', 'j', 'r', 'j'};
+
+		assertEquals(0, tile.getRotation());
+
+		tile.rotate();
+		assertEquals(90, tile.getRotation());
+		assertArrayEquals(rotatedSides1, tile.getSides());
+
+		tile.rotate();
+		assertEquals(180, tile.getRotation());
+		assertArrayEquals(rotatedSides2, tile.getSides());
+
+		tile.rotate();
+		assertEquals(270, tile.getRotation());
+		assertArrayEquals(rotatedSides3, tile.getSides());
+
+		tile.rotate();
+		assertEquals(0, tile.getRotation());
+		assertArrayEquals(originalSides, tile.getSides());
+
+		tile.rotate();
+		assertEquals(90, tile.getRotation());
+		assertArrayEquals(rotatedSides1, tile.getSides());
 	}
 }
