ce8fba8e4eda18409734ac9d109f4df1c5064e86
Kenan Yildirim <kenan@kenany.me>
2016-11-24 22:34:32 -0500

    possible ai interface?
    Not worthy yet for master branch.

---
+++ src/tiger_zone/ai/AiPlayer.java
@@ -0,0 +1,26 @@
+package tiger_zone.ai;
+
+import tiger_zone.Game;
+
+/**
+ * Abstract class for AI players to extend.
+ */
+public abstract class AiPlayer {
+	protected final Game game;
+	
+	/**
+	 * Creates a new instance of <code>AiPlayer</code>.
+	 * 
+	 * @param game The game instance.
+	 */
+	public AiPlayer(Game game) {
+		this.game = game;
+	}
+	
+	/**
+	 * Have this AI player make a move on the board.
+	 */
+	public void makeMove() {
+		// TODO: figure out if an implementation should be here?
+	}
+}
