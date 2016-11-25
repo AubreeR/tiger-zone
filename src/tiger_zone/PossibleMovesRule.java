74405c1112251ed72997a965c40e86dbacd5d9c8
Steven Remington <stevenrremington@gmail.com>
2016-11-25 17:59:00 -0500

    Improving time complexity for possible moves
    350 avg milliseconds down to 94

--- src/tiger_zone/PossibleMovesRule.java
+++ src/tiger_zone/PossibleMovesRule.java
@@ -1,13 +1,15 @@
 package tiger_zone;
 
-
+import java.util.ArrayList;
 
 public class PossibleMovesRule extends PlacementRule
 {
 	private Tile nextTile;//we must know the next tile to be placed in order to match sides
 	private boolean[][] north,west,south,east;//relative to all the different rotations
+	private boolean foundQuad1, foundQuad2, foundQuad3, foundQuad4;
 	private int length;
 	private int firstMove[][];
+	private ArrayList<int[]> possibleMoves;
 	
 	public PossibleMovesRule(Board boardState,int cartX, int cartY, Tile nextTile, boolean trace)
 	{
@@ -21,6 +23,7 @@ public class PossibleMovesRule extends PlacementRule
 		west  = new boolean[length][length];
 		south = new boolean[length][length];
 		east  = new boolean[length][length];
+		
 		firstMove = new int[4][3];
 		for(int i = 0; i < 4; i++)
 		{
@@ -28,6 +31,7 @@ public class PossibleMovesRule extends PlacementRule
 			firstMove[i][1] = Integer.MAX_VALUE;
 			firstMove[i][2] = Integer.MAX_VALUE;
 		}
+		possibleMoves = new ArrayList<int[]>();
 	}
 	public void output()
 	{
@@ -122,21 +126,30 @@ public class PossibleMovesRule extends PlacementRule
 					{
 					case 0: eval= boardState.validTilePlacement(i, j, this.nextTile, false);
 						north[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)] = eval;
+						if(eval)
+							possibleMoves.add(new int[]{i,j,0});
+						
 						setArray(eval, i, j);
 						ret = ret || north[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)];
 						break;
 					case 1:eval = boardState.validTilePlacement(i, j, this.nextTile, false);
 						west[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)] = eval;
+						if(eval)
+							possibleMoves.add(new int[]{i,j,90});
 						setArray(eval, i, j);
 						ret = ret || west[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)];
 						break;
 					case 2:eval = boardState.validTilePlacement(i, j, this.nextTile, false);
 						south[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)] = eval;
+						if(eval)
+							possibleMoves.add(new int[]{i,j,180});
 						setArray(eval, i, j);
 					ret = ret || south[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)];
 						break;
 					case 3:eval = boardState.validTilePlacement(i, j, this.nextTile, false);
 						east[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)] = eval;
+						if(eval)
+							possibleMoves.add(new int[]{i,j,270});
 						setArray(eval, i, j);
 						ret = ret || east[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)];
 						break;
@@ -159,19 +172,33 @@ public class PossibleMovesRule extends PlacementRule
 	{
 		if(eval)//the first true value
 		{
-		int i = -1;
-		if(x >= 0 && y >= 0)
-			i = 0;
-		else if(x <= 0 && y >= 0)
-			i = 1;
-		else if( x <= 0 && y <= 0)
-			i = 2;
-		else if( x >= 0 && y <= 0)
-			i = 3;
-		
-			firstMove[i][0] = x;
-			firstMove[i][1] = y;
-			firstMove[i][2] = this.nextTile.getRotation();
+			int i = -1;
+			if(x >= 0 && y >= 0 && !foundQuad1)
+			{
+				i = 0;
+				foundQuad1 = true;
+			}
+			else if(x <= 0 && y >= 0 && !foundQuad2)
+			{
+				i = 1;
+				foundQuad2 = true;
+			}
+			else if( x <= 0 && y <= 0 && !foundQuad3)
+			{
+				i = 2;
+				foundQuad3 = true;
+			}
+			else if( x >= 0 && y <= 0 && !foundQuad4)
+			{
+				i = 3;
+				foundQuad4 = true;
+			}
+			if( i >= 0)
+			{
+				firstMove[i][0] = x;
+				firstMove[i][1] = y;
+				firstMove[i][2] = this.nextTile.getRotation();
+			}
 		}
 	}
 	
@@ -191,6 +218,106 @@ public class PossibleMovesRule extends PlacementRule
 		return ret;
 	}
 	
+	public int[] getClosestToOriginEstimateMove()
+	{
+		int choice = -1;
+		double distance = Double.MAX_VALUE;
+		double temp = Double.MAX_VALUE;
+		for(int i = 0; i < firstMove.length; i++)
+		{
+			if(this.firstMove[i][0] == Integer.MAX_VALUE)
+				continue;
+			temp = Math.sqrt(Math.pow(this.firstMove[i][0], 2) + Math.pow(this.firstMove[i][1], 2));
+			if(temp < distance)
+			{
+				choice = i;
+				distance = temp;
+			}
+		}
+		return this.firstMove[choice];
+	}
+	
+	public int[] getClosestToOriginMove()
+	{
+		int choice = -1;
+		int rot = Integer.MAX_VALUE;
+		int x = Integer.MAX_VALUE;
+		int y = Integer.MAX_VALUE;
+		double distance = Double.MAX_VALUE;
+		double temp = Double.MAX_VALUE;
+		
+		for(int r = 0; r < 4; r++)
+		{
+			for(int i = -((length -1)/ 2); i < length/2; i++)
+			{
+				for(int j = -((length-1) / 2); j < length/2; j++)
+				{
+					switch(r)
+					{
+					case 0: if(north[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)])
+						{
+							temp = distanceFromOrigin(i,j);
+							if(temp < distance)
+							{
+								distance = temp;
+								x = i;
+								y = j;
+								rot = 0;
+							}
+						}
+						break;
+					case 1:
+						if(west[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)])
+						{
+							temp = distanceFromOrigin(i,j);
+							if(temp < distance)
+							{
+								distance = temp;
+								x = i;
+								y = j;
+								rot = 0;
+							}
+						}
+						break;
+					case 2:
+						if(south[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)])
+						{
+							temp = distanceFromOrigin(i,j);
+							if(temp < distance)
+							{
+								distance = temp;
+								x = i;
+								y = j;
+								rot = 0;
+							}
+						}
+						break;
+					case 3:
+						if(east[boardState.getBoardPosX(i)][boardState.getBoardPosY(j)])
+						{
+							temp = distanceFromOrigin(i,j);
+							if(temp < distance)
+							{
+								distance = temp;
+								x = i;
+								y = j;
+								rot = 0;
+							}
+						}
+						break;
+					}
+				
+				}
+			}
+		}
+		int[] ret = {x, y, rot};
+		return ret;
+	}
+	public double distanceFromOrigin(int x, int y)
+	{
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(x, 2));
+	}
+	public ArrayList<int[]> getAllPossibleMoves() { return possibleMoves; }
 	public boolean[][] get0Rotation() { return this.north; }
 	public boolean[][] get_90Rotation(){ return this.west; }
 	public boolean[][] get_180Rotation(){return this.south; }
@@ -203,4 +330,6 @@ public class PossibleMovesRule extends PlacementRule
 	}
 	
 }
+
+
 
