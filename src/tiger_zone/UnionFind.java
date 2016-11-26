291f3e7a36f6f50646779971329b4cd23d460121
Steven Remington <stevenrremington@gmail.com>
2016-11-25 22:00:59 -0500

    Almost Working Union Find for internal connectedness
    doesn't work for the horrendous CASES

--- src/tiger_zone/UnionFind.java
+++ src/tiger_zone/UnionFind.java
@@ -16,6 +16,7 @@ public class UnionFind//used to find what areas on a tile are connected adn the 
 		for(int i = 0; i < parent.length; i++)
 			parent[i] = i;
 		this.evaluateAdjacentCase();
+		this.evaluateEdgeCase();
 	}
 	
 	public void evaluateAdjacentCase()
@@ -41,12 +42,28 @@ public class UnionFind//used to find what areas on a tile are connected adn the 
 		//	0	1	2
 		//	3	4	5
 		//	6	7	8
-		if(tile.getSide(1) == 'l')
 		
-		for(int i = 1; i <= 7; i += 2)
+		
+		for(int i = 0; i < 4; i++)
 		{
-			if(tile.getZone(i) == 'l')
+			if(tile.getSide(i) == 'l')
 			{
+				int j = (i != 3) ? i+1 : 0;
+				int k = (i != 0) ? i-1 : 3;
+				if(tile.getSide(j) != 'l' && tile.getSide(k) != 'l')
+				{
+					int eight = (i == 2) ? 8 : 0;
+					int four  = (i % 2 == 1) ? 4 : 0;
+					int two   = (i < 2) ? 2 : 0;
+					int one   = 0;
+					int tot = eight + four + two + one;
+					if(i % 2 == 0 && tile.getZone(tot + 1) == 'j' && tile.getZone(tot - 1) == 'j')
+						this.union(tot +1 - 1, tot - 1 - 1);
+					char up = tile.getZone(tot + 3);
+					char down = tile.getZone(tot - 3);
+					if(i % 2 == 1 && tile.getZone(tot + 3) == 'j' && tile.getZone(tot - 3) == 'j')
+						this.union(tot + 3 - 1,  tot - 3- 1);
+				}
 				
 			}
 		}
--- src/tiger_zone/ui/Main.java
+++ src/tiger_zone/ui/Main.java
@@ -14,11 +14,33 @@ import tiger_zone.Client;
 import tiger_zone.Game;
 import tiger_zone.Player;
 import tiger_zone.Tile;
+import tiger_zone.UnionFind;
 import tiger_zone.ai.PoorAiPlayer;
 import tiger_zone.Protocol;
 
 public class Main {
 	public static void main(String[] args) {
+		char[] Qsides = {'j','l','t','t'};
+		char[] Qtigers = {'j','=','=','t','=','l','j','=','='};
+		char[] Qcrocs = {'=','=','j','t','=','=','=','=','='};
+		Tile t = new Tile( Qsides, '-', Qtigers, Qcrocs, "./src/resources/tile17.png");
+		t.rotate();
+		t.rotate();
+		UnionFind uf = new UnionFind(t);
+		for(int i = 0; i < 9; i++)
+		{
+			if(i % 3 == 0 && i != 0)
+				System.out.println("");
+			System.out.print(t.getZone(i+1) + "\t");
+		}
+		System.out.println("");
+		for(int i = 0; i < 9; i++)
+		{
+			if(i % 3 == 0 && i != 0)
+				System.out.println("");
+			System.out.print(uf.findRoot(i) + "\t");
+		}
+		System.out.println("");
 		
 //		Stack<Tile> pile = Board.createDefaultStack();
 //		Collections.shuffle(pile);
