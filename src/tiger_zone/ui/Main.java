803525d7938f92a7f58c4218ce9b71529da1b2f3
Steven Remington <stevenrremington@gmail.com>
2016-11-26 16:02:45 -0500

    sadsad

--- src/tiger_zone/ui/Main.java
+++ src/tiger_zone/ui/Main.java
@@ -20,14 +20,53 @@ import tiger_zone.Protocol;
 
 public class Main {
 	public static void main(String[] args) {
-	
-
+		char[] Qsides = {'j','l','t','t'};
+		char[] Qtigers = {'j','=','=','t','=','l','j','=','='};
+		char[] Qcrocs = {'=','=','j','t','=','=','=','=','='};
+		Tile t = new Tile( Qsides, '-', Qtigers, Qcrocs, "./src/resources/tile17.png");
+		t.rotate();
+		t.rotate();
+		UnionFind uf = new UnionFind(t);
+		for(int i = 0; i < 9; i++)
+		{
+			if(i % 3 == 0 && i != 0)
+				System.out.println("");
+			System.out.print(t.getZone(i+1) + "\t");
+		}
+		System.out.println("");
+		for(int i = 0; i < 9; i++)
+		{
+			if(i % 3 == 0 && i != 0)
+				System.out.println("");
+			System.out.print(uf.findRoot(i) + "\t");
+		}
+		System.out.println("");
+		
+//		Stack<Tile> pile = Board.createDefaultStack();
+//		Collections.shuffle(pile);
+//		Game game = new Game(pile);
+//
+//
+//		//Protocol p = new Protocol("0.0.0.0",8000,"tpass","user","pass");
+//		//p.tournamentProtocol();
+//
+//		List<Player> players = new ArrayList<Player>(2);
+//		players.add(new Player(0));
+//		players.add(new Player(1));
+//		game.setPlayers(players);
+//
+//		BoardFrame bf = new BoardFrame(game);
+//		bf.setSize(900, 900);
+//		bf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		bf.setVisible(true);
 		
 		Stack<Tile> pile = Board.createDefaultStack();
 		Collections.shuffle(pile);
 		Game game = new Game(pile);
 
-
+		
+		//Protocol p = new Protocol("0.0.0.0",8000,"tpass","user","pass");
+		//p.tournamentProtocol();
 
 		List<Player> players = new ArrayList<Player>(2);
 		players.add(new Player(0, "p1"));
