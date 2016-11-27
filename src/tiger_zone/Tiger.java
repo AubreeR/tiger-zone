a3845739a3a22929092d6d1f96254a855b228f8d
Kenan Yildirim <kenan@kenany.me>
2016-11-26 22:54:53 -0500

    remove all unused methods from Tiger
    None of this stuff is used anywhere else in the codebase.

--- src/tiger_zone/Tiger.java
+++ src/tiger_zone/Tiger.java
@@ -1,69 +1,27 @@
 package tiger_zone;
 
-enum TigerPlacement {
-	JUNGLE,
-	LAKE,
-	GAME,
-	DEN, 
-	NOTPLACED
-};
-
 /**
  * The <code>Tiger</code> class represents a tiger, which may be used by a <code>Player</code> to claim completeable
- * features.
+ * features (jungles, lakes, game trails, and dens).
  */
 public class Tiger {
-	private int xCoor, yCoor;//coordinates of tile on the board(for the purpose of redundancy)
-	private Tile tile;//the tile it is placed on
-	private int locationPlaced;//what array index is the tiger placed on
-	private TigerPlacement placement;//the role fo the tiger
-	private int tigerCount;//Can use a max of 7 tigers
+	/**
+	 * The owner of this tiger.
+	 */
 	private final Player owner;
-	
+
+	/**
+	 * Creates a new instance of tiger with the specified owner.
+	 *
+	 * @param owner Player instance that owns this tiger.
+	 */
 	public Tiger(final Player owner) {
-		xCoor = -1;
-		yCoor = -1;
-		locationPlaced = -1;
-		tile = null;
-		placement = TigerPlacement.NOTPLACED;
 		this.owner = owner;
 	}
-	
-	/**
-	 * Looks at the given tile coordinate the tiger was placed on to figure out it's role
-	 * @return TigerPlacement value (i.e. Jungle, Lake, Game...)
-	 * @throws Exception if it is placed in a blatantly 
-	 */
-	TigerPlacement evaluateRole() throws Exception
-	{
-		switch(tile.getSides()[this.locationPlaced])
-		{
-		case 'j':	// j = jungle
-			return TigerPlacement.JUNGLE;
-		case 'l':	// l = lake
-			return TigerPlacement.LAKE;
-		case 't' :	// t = game trail
-			return TigerPlacement.GAME;
-		case 'x':	// x = den
-			return TigerPlacement.DEN;
-		case 'e'://cannot place a tiger on e
-		default:
-			
-			throw new Exception("INVALID TIGER PLACEMENT: attempted to be placed on a location that doesn't allow a tiger.");
-		
-		}
-	
-	}
-	
-	TigerPlacement getTigerPlacement() { return placement; }
-	int getLocation() { return locationPlaced; }
-	int getXCoor() { return xCoor; }
-	int getYCoor() { return yCoor; }
-	Tile getTile() { return tile;  }
-	
+
 	/**
 	 * Returns this tiger's owner.
-	 * 
+	 *
 	 * @return owner
 	 */
 	public final Player getOwner() {
