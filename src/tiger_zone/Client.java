e5571043a99126fa84eb4bb05ae5f47b620ca851
Kenan Yildirim <kenan@kenany.me>
2016-12-01 20:09:43 -0500

    clean up Client

--- src/tiger_zone/Client.java
+++ src/tiger_zone/Client.java
@@ -7,9 +7,9 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Client {
-	protected String serverName;//ip address of server
-	protected int portNumber;//server port
+public final class Client {
+	private final String address;//ip address of server
+	private final int portNumber;//server port
 
 	private Socket echoSocket;//server connection object
 	private PrintWriter output;//used to send data to server
@@ -17,37 +17,31 @@ public class Client {
 
 	public Client()
 	{
-		serverName = new String ("0.0.0.0");
-		portNumber = 0;
-		System.out.println ("Attemping to connect to host " +
-				serverName + " on port " + portNumber);
-
-		echoSocket = null;
-		output = null;
-		input = null;
-		stdIn = null;
+		this.address = "0.0.0.0";
+		this.portNumber = 0;
+		this.echoSocket = null;
+		this.output = null;
+		this.input = null;
+		this.stdIn = null;
 	}
 
 	/**
 	 * primary constructor that initializes the Client with usable server info
-	 * @param serverName IP address of the server
+	 * 
+	 * @param address IP address of the server
 	 * @param portNumber port that the tcp communication works through
 	 */
-	public Client(String serverName, int portNumber)
+	public Client(String address, int portNumber)
 	{
-		this.serverName = serverName;
+		this.address = address;
 		this.portNumber = portNumber;
-		System.out.println ("Attemping to connect to host " +
-				serverName + ":" + portNumber);
-
-		echoSocket = null;
-		output = null;
-		input = null;
-		stdIn = null;
+		this.echoSocket = null;
+		this.output = null;
+		this.input = null;
+		this.stdIn = null;
 
 		//tries to connect the server, if it connects do logic
-		connect(this.serverName, this.portNumber);
-
+		this.connect();
 	}
 
 	/**
@@ -56,26 +50,27 @@ public class Client {
 	 * @param portNumber the port that the tcp socket communicates with
 	 * @return true if the connection was successful, false if it failed
 	 */
-	public boolean connect(String serverName, int portNumber)
+	private final boolean connect()
 	{
+		System.out.println("Attemping to connect to host " + this.address + ":" + this.portNumber);
+		
 		try {
 			//getting the socket to the server
-			echoSocket = new Socket(serverName,portNumber);
+			this.echoSocket = new Socket(this.address, this.portNumber);
 			//out is used to send information to the server
-			output = new PrintWriter(echoSocket.getOutputStream(), true);
+			this.output = new PrintWriter(this.echoSocket.getOutputStream(), true);
 			//inis used to recieve information from the server
-			input = new BufferedReader(new InputStreamReader(
-					echoSocket.getInputStream()));
+			this.input = new BufferedReader(new InputStreamReader(this.echoSocket.getInputStream()));
 			//stdIn is used to get user input
-			stdIn = new BufferedReader(
-					new InputStreamReader(System.in));
-			System.out.println("Client connection to " + serverName + ":" + portNumber + (this.isConnected() ? " succeeded." : " failed."));
+			this.stdIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Client connection to " + this.address + ":" + this.portNumber
+					+ (this.isConnected() ? " succeeded." : " failed."));
 			return true;
 		} catch (UnknownHostException e) {
-			System.err.println("Host Does Not Exist: " + serverName);
+			System.err.println("Host does not exist: " + this.address);
 			return false;
 		} catch (IOException e) {
-			System.err.println("Couldn't get I/O for the connection to: " + serverName);
+			System.err.println("Couldn't get I/O for the connection to: " + this.address);
 			return false;
 		}
 	}
@@ -83,13 +78,13 @@ public class Client {
 	/**
 	 * closes out of the streams and disconnects from the srever
 	 */
-	public void disconnect()
+	public final void disconnect()
 	{
-		try{
-			output.close();
-			input.close();
-			stdIn.close();
-			echoSocket.close();
+		try {
+			this.output.close();
+			this.input.close();
+			this.stdIn.close();
+			this.echoSocket.close();
 		}
 		catch(IOException io)
 		{
@@ -101,19 +96,19 @@ public class Client {
 	 * Sends data to the server over a PrintWriter
 	 * @param input the string value to send to the server
 	 */
-	public void sendToServer(String input)
+	public final void sendToServer(String input)
 	{
-		output.println(input.trim());
+		this.output.println(input.trim());
 	}
 
 	/**
 	 * Gets a string from the server
 	 * @return the servers response, null if there is a disconnection
 	 */
-	public String receiveFromServer()
+	public final String receiveFromServer()
 	{
-		try{
-			String response = input.readLine().trim();
+		try {
+			String response = this.input.readLine().trim();
 			System.out.printf("Server: %s\n", response);
 			return response;
 		}
@@ -128,24 +123,10 @@ public class Client {
 	 * isConnected tests if the socket connection is still usable
 	 * @return true if socket connection is usable, false if it is dead
 	 */
-	public boolean isConnected()
+	private final boolean isConnected()
 	{
 		//a socket will remain "connected" state if it is just closed
 		//so we have to ensure that it is connected and currently open
-		return echoSocket.isConnected() && (!echoSocket.isClosed());
-	}
-
-	/**
-	 * 
-	 * @return the name of the active 
-	 */
-	public String getServerName()
-	{
-		return serverName;
-	}
-
-	public int getPortNumber()
-	{
-		return portNumber;
+		return this.echoSocket.isConnected() && (!this.echoSocket.isClosed());
 	}
 }
