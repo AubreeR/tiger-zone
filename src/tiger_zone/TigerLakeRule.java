8ea4c2aeeabf183e0e853d5beeaab16bee945fa5
Kenan Yildirim <kenan@kenany.me>
2016-12-02 11:17:54 -0500

    make TigerLakeRule methods final

--- src/tiger_zone/TigerLakeRule.java
+++ src/tiger_zone/TigerLakeRule.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class TigerLakeRule extends PlacementRule {
-	private Tile tilePlaced;
-	private Set<Position> visited = new HashSet<Position>();
-	private ArrayList<Position> usedTiles;
-	private int zone;
-	
-	
+public final class TigerLakeRule extends PlacementRule {
+	private final Tile tilePlaced;
+	private final ArrayList<Position> usedTiles;
+	private final int zone;
+
+
 	public TigerLakeRule(Board boardState, Position position, Tile tilePlaced, int zone) {
 		super(boardState, position, true);
 		super.setRuleName("TigerLake Rule");
@@ -18,7 +17,7 @@ public class TigerLakeRule extends PlacementRule {
 		this.zone = zone;
 		this.usedTiles = new ArrayList<Position>();
 	}
-	
+
 	public TigerLakeRule(Board boardState, Position position, Tile tilePlaced, int zone, boolean trace) {
 		super(boardState, position, trace);
 		super.setRuleName("TigerLake Rule");
@@ -26,9 +25,9 @@ public class TigerLakeRule extends PlacementRule {
 		this.zone = zone;
 		this.usedTiles = new ArrayList<Position>();
 	}
-	
+
 	@Override
-	public boolean evaluate() {
+	public final boolean evaluate() {
 		try {
 			if(checkChildren(this.position)) {
 				if(trace) {
@@ -38,7 +37,7 @@ public class TigerLakeRule extends PlacementRule {
 					}
 				}
 				return true;
-				
+
 			}
 			else {
 				testFailure();
@@ -53,29 +52,30 @@ public class TigerLakeRule extends PlacementRule {
 		return false;
 	}
 
-	public boolean checkChildren(Position position) {
+	public final boolean checkChildren(Position position) {
 		Set<Position> testedTiles = new HashSet<Position>();
 		if (this.tilePlaced.getZone(this.zone) != 'l'|| this.tilePlaced.hasTiger()) {
 			 return false;
 		}
 		return recursiveLake(testedTiles, position);
 	}
-	
-	public boolean recursiveLake(Set<Position> testedTiles, Position position) {
+
+	public final boolean recursiveLake(Set<Position> testedTiles, Position position) {
 		if(boardState.getTile(position) == null) {
 			return true;
 		}
-		
-		//mark adjacent tiles with lakes
+
+		// mark adjacent tiles with lakes
 		if(boardState.getTile(position).hasTiger()) {
 			return false;
 		}
-		
+
+
 		//already visited this tile or no more tiles to check
 		if(testedTiles.contains(position)) {
 			return true;
 		}
-		
+
 		boolean ret = true;
 		for (int i = 0; i < 4; i++) {
 			if (boardState.getTile(position).getSide(i) == 'l') {
@@ -84,26 +84,26 @@ public class TigerLakeRule extends PlacementRule {
 					testedTiles.add(position);
 				}
 				switch(i) {
-					case 0:	
+					case 0:
 						ret = ret && recursiveLake(testedTiles, position.north());
 						break;
-					case 1: 
+					case 1:
 						ret = ret && recursiveLake(testedTiles, position.east());
 						break;
-					case 2: 
+					case 2:
 						ret = ret && recursiveLake(testedTiles, position.south());
 						break;
-					case 3: 
+					case 3:
 						ret = ret && recursiveLake(testedTiles, position.west());
 						break;
 					default:
-						break;	
-				}	
+						break;
+				}
 			}
 		}
 		return ret;
 	}
-	
+
 	@Override
 	public void testFailure() throws Exception {
 		throw new Exception(super.getName() + " failed under condition lake is not complete");
