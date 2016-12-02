415b21aa218f125c263edce7c878ad5625189070
Kenan Yildirim <kenan@kenany.me>
2016-12-01 21:24:21 -0500

    simplify evaluateRules

--- src/tiger_zone/RuleEngine.java
+++ src/tiger_zone/RuleEngine.java
@@ -2,7 +2,9 @@ package tiger_zone;
 
 import java.util.ArrayList;
 
-//Manages rules and validation of tile placement
+/**
+ * Manages rules and validation of tile placement.
+ */
 public class RuleEngine
 {
 	ArrayList<Rule> ruleList;
@@ -10,14 +12,14 @@ public class RuleEngine
 	{
 		ruleList = new ArrayList<Rule>();
 	}
-	
+
 	//Description: adds a rule to to the existing set of tests
 	//returns: void
 	public void addRule(Rule gameRule)
 	{
-		ruleList.add(gameRule);	
+		ruleList.add(gameRule);
 	}
-	
+
 	//Description: removes a rule from the set of rules to be tested
 	//	uses the name of the rule to find it
 	//returns: 	true == rules successfully found and removed
@@ -31,48 +33,48 @@ public class RuleEngine
 				ruleList.remove(i);
 				flagReturn = true;
 			}
-				
+
 		return flagReturn;
 	}
-	
+
 	public void clearRules()
 	{
 		ruleList = new ArrayList<Rule>();
 	}
-	
+
 	//Description: test a specific subset of the rules
 	//returns: 	true == all rules passes
 	//			false == a rule failed
 	public boolean evaluateRules(ArrayList<String> ruleSet)
 	{
 		boolean flagReturn = true;
-		//should probably implement a find functionality that 
+		//should probably implement a find functionality that
 		//isn't n^2
 		for(String name : ruleSet)
 		{
 			for(Rule rule : ruleList)
 			{
-			
+
 				if(rule.getName().compareTo(name) == 0)
 				{
 					//allows us to run through all evaluations but still
 					//retain failure status
 					flagReturn = rule.evaluate() && flagReturn;
-					 
+
 				}
-				
+
 			}
 		}
-		
+
 		return flagReturn;
 	}
-	
+
 	//Description: test a specific subset of the rules with the same name
 	//returns: 	true == all rules passes
 	//			false == a rule failed
 	public boolean evaluateRules(String testRule)
 	{
-	    boolean flagReturn = true;	
+		boolean flagReturn = true;
 		for(Rule rule : ruleList)
 		{
 			if(flagReturn)
@@ -80,7 +82,7 @@ public class RuleEngine
 				if(testRule.compareTo(rule.getName()) == 0)
 				{
 					flagReturn = rule.evaluate() && flagReturn;
-					
+
 				}
 			}
 			else
@@ -88,28 +90,18 @@ public class RuleEngine
 		}
 		return flagReturn;
 	}
-	
+
 	//Description: test the entire set of existing rules
 	//returns: 	true== all rules passed
 	//       	false == a rule failed
 	public boolean evaluateRules()
 	{
-		boolean flagReturn = true;
-		
-		for(Rule rule : ruleList)
+		for (Rule rule : ruleList)
 		{
-			
-			if(flagReturn)
-			{
-				
-				flagReturn = rule.evaluate() && flagReturn;
+			if (!rule.evaluate()) {
+				return false;
 			}
-			else
-				break;
 		}
-		return flagReturn;
+		return true;
 	}
-	
-
-	
 }
